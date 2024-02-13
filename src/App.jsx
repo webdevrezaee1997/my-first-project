a0a22024cfcb7d6ad012263743201f89a853b211
webdevrezaee1997 <webdevrezaee1997@gmail.com>
2024-02-13 20:41:50 +0330

    my first project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import MyFirstApp from "./Components/MyFirstApp.jsx";
+function App() {
+  return (
+    <>
+      <MyFirstApp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MyFirstApp.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+const MyFirstApp = () => {
+    const arr=[{name:'task1',number:'point'},{name:'task2', number:'point'}]
+    
+  return (
+  
+    <div style={{  display:"flex",
+  justifyContent:"center",alignItems:"center" , }}>
+       <div className="card bg-success" style={{display:"flex",
+textAlign:"center",alignItems:"center",width:"100%",height:"100%",minHeight:200,border:"1px solid black",color:"black",}}>
+  <h1 className='text-white'>Login page</h1>
+    <input placeholder='type username' type="text" className="text-success w-50 rounded-2 mb-1" />
+    <input  placeholder='type password'  type="text" className="text-success w-50 rounded-2 mt-1" />
+    <input type="submit" className='bg-primary w-50 rounded-2 mt-1' />
+    <h1>My First Todo</h1>
+    
+    {arr.map((x)=>(
+        
+        <div  className=''>
+             <h3 className=' mb-3  mt-1 p-0  '>{x.name}</h3> 
+             <h5 className=' mt-3 mb-1 p-0 text-white '>{x.number}</h5>
+        </div>
+    
+    ))}
+</div>
+ 
+    </div>
+   
+)};
+
+MyFirstApp.propTypes = {};
+
+export default MyFirstApp;
